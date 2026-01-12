fa65c2bf69a3772469e2e1b64d1b0210f6695301
myanwc <anton.kostyrev@yandex.ru>
2026-01-12 11:25:32 +0300

    Fix goglevideo bypass metod, make output easyer

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <thread>
 #include <chrono>
 
+
 int main() {
     if (!std::filesystem::exists(std::filesystem::current_path() / "tls_clienthello_www_google_com.bin")) {
         std::cout << "Can't find tls_clienthello_www_google_com.bin file\n"
@@ -11,31 +12,30 @@ int main() {
             << "And put it here -> " << std::filesystem::current_path().string() << "\n";
     }
     else {
-        std::cout << "Getting ready...\n";
         DPIBypasser bypasser("outbound");
-        std::cout << "Setup bypass rules...\n";
         bypasser.AddBypassRequiredHostname("osu.direct", BypassMethod::SIMPLE_SNI_FAKE);
         bypasser.AddBypassRequiredHostname("catboy.best", BypassMethod::SIMPLE_SNI_SPLIT);
         bypasser.AddBypassRequiredHostname("api.nerinyan.moe", BypassMethod::SIMPLE_SNI_FAKE);
 
-        bypasser.AddBypassRequiredHostname("www.youtube.com", BypassMethod::SSF_FAKED_SPLIT);
         bypasser.AddBypassRequiredHostname("youtube.com", BypassMethod::SSF_FAKED_SPLIT);
-        bypasser.AddBypassRequiredHostname("googlevideo.com", BypassMethod::SSF_FAKED_SPLIT);
+        bypasser.AddBypassRequiredHostname("googlevideo.com", BypassMethod::SIMPLE_SNI_FAKE);
         bypasser.AddBypassRequiredHostname("i.ytimg.com", BypassMethod::SSF_FAKED_SPLIT);
         bypasser.AddBypassRequiredHostname("yt3.ggpht.com", BypassMethod::SSF_FAKED_SPLIT);
         bypasser.AddBypassRequiredHostname("i9.ytimg.com", BypassMethod::SSF_FAKED_SPLIT);
+        bypasser.AddBypassRequiredHostname("doubleclick.net", BypassMethod::SSF_FAKED_SPLIT);
+        bypasser.AddBypassRequiredHostname("cloudflare-dns.com", BypassMethod::SSF_FAKED_SPLIT);
 
         bypasser.AddBypassRequiredHostname("discord.com", BypassMethod::SSF_FAKED_SPLIT);
         bypasser.AddBypassRequiredHostname("discord", BypassMethod::SSF_FAKED_SPLIT);
         bypasser.AddBypassRequiredHostname("discord.gg", BypassMethod::SSF_FAKED_SPLIT);
         bypasser.AddBypassRequiredHostname("discord.media", BypassMethod::SSF_FAKED_SPLIT);
         bypasser.AddBypassRequiredHostname("mtalk.google.com", BypassMethod::SSF_FAKED_SPLIT);
-        bypasser.AddBypassRequiredHostname("cloudflare-ech.com", BypassMethod::SSF_FAKED_SPLIT);
         bypasser.AddBypassRequiredHostname("login.live.com", BypassMethod::SSF_FAKED_SPLIT);
 
-        std::cout << "Setted up!\n";
+        bypasser.AddBypassRequiredHostname("tryhackme", BypassMethod::SSF_FAKED_SPLIT);
+
         try {
-            std::cout << "5 seconds before start\nWindow will be hidden!\nRun KillProcess.bat for close DPIbypass" << std::endl;
+            std::cout << "5 seconds before start\nWindow will be hidden!\n!For close - run KillProcess.bat as admin!" << std::endl;
             std::this_thread::sleep_for(std::chrono::seconds(5)); // For the user to read the information
             ShowWindow(GetConsoleWindow(), SW_HIDE); // Hide window
             bypasser.Start();
